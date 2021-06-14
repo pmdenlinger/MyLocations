955e3af3318b98135b0cbbe4364524b3d9a2aee0
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-14 15:54:47 -0700

    Add ivars to VC.

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -8,10 +8,24 @@
 import UIKit
 
 class CurrentLocationViewController: UIViewController {
+    
+    @IBOutlet var messageLabel: UILabel!
+    @IBOutlet var latitudeLabel: UILabel!
+    @IBOutlet var longitudeLabel: UILabel!
+    @IBOutlet var addressLabel: UILabel!
+    @IBOutlet var tagButton: UIButton!
+    @IBOutlet var getButton: UIButton!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+//    MARK: -Actions
+    @IBAction func getLocation() {
+//        Do nothing yet
     }
 
 
